e1c4a699026cc0edb125c2f96a4712e8c929a1e2
Cristian Duvan Machado Mosquera <62184928+CriistiianDM@users.noreply.github.com>
2022-11-22 15:27:27 -0500

    cajas dinamicamente pero falta el efecto hover para crear la caja que mostrara las estadisticas

--- js/main.js
+++ js/main.js
@@ -37,12 +37,25 @@ function get_filt_pokemon(data_array) {
         var data_pokemon = new Object();
         var name_pokemon = data_array.data;
 
-        (name_pokemon).map( async (element) => {
-            
+        (name_pokemon).map(async (element) => {
+
             let data = await get_data_pokemon(element.name)
-     
+            console.log(data);
+            //agregar a una caja elementos
+            document.getElementsByClassName("box-container-elements")[0].innerHTML += `
+            <a class="box-cart-pokemon">
+                    <div class="circle-red-pokemon">
+                        <div class="circle-white-pokemon">
+                            <img class="img-pokemon" src="./img/${(data.forms)[0].name}.png" />
+                        </div>
+                     </div>
+                    <div class="box-title">
+                        <p class="title-pokemon">${(data.forms)[0].name}</p>
+                    </div>
+            </a>`
+
         });
-    
+
     } catch (error) {
         console.log(error);
     }
